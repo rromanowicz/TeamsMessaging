e497134f7048670865418ad7a5958f6053585b3c
rromanowicz <rromanowicz@hotmail.com>
2023-03-14 11:05:48 +0100

    Added message wrapper

--- src/main/java/ex/rr/teamsmessaging/adaptivecard/AdaptiveTemplate.java
+++ src/main/java/ex/rr/teamsmessaging/adaptivecard/AdaptiveTemplate.java
@@ -16,6 +16,7 @@ import static ex.rr.teamsmessaging.adaptivecard.constants.Templates.*;
 
 @Slf4j
 @Component
+@SuppressWarnings("unused")
 public class AdaptiveTemplate {
 
     private final Object obj;
@@ -23,23 +24,30 @@ public class AdaptiveTemplate {
     private final Style style;
     private String title;
 
+    private final Boolean wrapMessage;
 
-    private AdaptiveTemplate(Object obj, Map<String, String> facts, Style style) {
+
+    private AdaptiveTemplate(Object obj, Map<String, String> facts, Style style, Boolean wrapMessage) {
         this.obj = obj;
         this.facts = facts;
         this.style = style;
+        this.wrapMessage = wrapMessage;
     }
 
     public String apply() {
         String processObject = processObject(obj);
-        if(processObject.isBlank()){
+        if (processObject.isBlank()) {
             log.error("Cannot process element of type [{}].", obj.getClass().getSimpleName());
             throw new UnsupportedOperationException("Cannot process element.");
         }
         String output = T_CONTAINER.replace(R_FACTS, processFacts());
         output = output.replace(R_BODY, processObject);
 
-        return T_ADAPTIVE_CARD.replace(R_CONTAINER, output);
+        String adaptiveCard = T_ADAPTIVE_CARD.replace(R_CONTAINER, output);
+        if (wrapMessage) {
+            return T_MESSAGE.replace(R_VALUE, adaptiveCard);
+        }
+        return adaptiveCard;
     }
 
     private String processFacts() {
@@ -111,7 +119,7 @@ public class AdaptiveTemplate {
                 }
             } else {
                 String nestedObject = processObject(v);
-                if(nestedObject.isBlank()) return;
+                if (nestedObject.isBlank()) return;
                 cellValue = nestedObject;
             }
 
@@ -154,6 +162,8 @@ public class AdaptiveTemplate {
         private Object obj;
         private Style style = Style.DEFAULT;
 
+        private Boolean wrapMessage = false;
+
         public Builder object(Object obj) {
             this.obj = obj;
             return this;
@@ -169,8 +179,13 @@ public class AdaptiveTemplate {
             return this;
         }
 
+        public Builder addWrapper() {
+            this.wrapMessage = true;
+            return this;
+        }
+
         public AdaptiveTemplate build() {
-            return new AdaptiveTemplate(obj, facts, style);
+            return new AdaptiveTemplate(obj, facts, style, wrapMessage);
         }
     }
 
